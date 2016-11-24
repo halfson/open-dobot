83bbd5ed1e5b6e7fab43846832144d945d492050
Hakan <hakanyarimoglu@gmail.com>
2016-11-24 10:16:49 +0300

    Limit switch activated
    The first code that limit switch works

--- application/python/example-limit-switch.py
+++ application/python/example-limit-switch.py
@@ -12,7 +12,21 @@ calibration for Joint1 (base), which originally has no sensors.
 You can get an interrupter here:
 https://www.sparkfun.com/products/9299
 https://www.sparkfun.com/products/9322
-
+		Initiates joint calibration procedure using a limit switch/photointerrupter. Effective immediately.
+		Current command buffer is cleared.
+		Cancel the procedure by issuing EmergencyStop() is necessary.
+		
+		@param joint - which joint to calibrate: 1-3
+		@param forwardCommand - command to send to the joint when moving forward (towards limit switch);
+				use freqToCmdVal()
+		@param backwardCommand - command to send to the joint after hitting  (towards limit switch);
+				use freqToCmdVal()
+		@param direction - direction to move joint towards limit switch/photointerrupter: 0-1
+		@param pin - firmware internal pin reference number that limit switch is connected to;
+					refer to dobot.h -> calibrationPins
+		@param pinMode - limit switch/photointerrupter normal LOW = 0, normal HIGH = 1
+		@param pullup - enable pullup on the pin = 1, disable = 0
+		@return True if command succesfully received, False otherwise.
 '''
 
 from dobot import DobotDriver
@@ -26,4 +40,6 @@ driver.Open()
 # Switch is expected to be connected (soldered) to pin D8 and pulled up (HIGH) externally (with a
 # resistor, e.g. 4.7k, to 5V supply) or be an active device (like a photointerrupter). Pullup is not enabled on that pin.
 
-driver.CalibrateJoint(1, driver.freqToCmdVal(1000), driver.freqToCmdVal(50), 1, 5, 1, 0)
+# CalibrateJoint(self, joint, forwardCommand, backwardCommand, direction, pin, pinMode, pullup)
+# driver.CalibrateJoint(1, driver.freqToCmdVal(1000), driver.freqToCmdVal(50),1,5, 0, 0) İlk çalışan manuel switch ile çalışan kod.
+driver.CalibrateJoint(1, driver.freqToCmdVal(1000), driver.freqToCmdVal(50),1 , 5, 0, 0)
